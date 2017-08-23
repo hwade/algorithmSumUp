b26a62f5028934fb44b102081a43be5a3d391b14
hwade <hwade_good@163.com>
2017-08-23 19:55:20 +0800

    求1/1!-1/3!+1/5!-1/7!+.....+(-1)^(n+1)/(2n-1)!

--- sumsCyclicFactorial.c
+++ sumsCyclicFactorial.c
@@ -2,7 +2,6 @@
 /* 
   题目：求1/1!-1/3!+1/5!-1/7!+.....+(-1)^(n+1)/(2n-1)!
   思路：
-<<<<<<< HEAD
     1.累乘，每次结果累加
 */
 
@@ -17,31 +16,6 @@ float add(int n){
         sum += ((i%2 == 0)?-1.0:1.0)/denominator;
     }
     return sum;
-=======
-    1.求阶乘
-    2.当前一次循环已经求出7!时,再去求9!不需要从1累成，从上一次7!的结果继续乘
-    3.时间复杂度从O(n2)变为O(n)
-*/
-float fact(float n, int tag){
-    if(tag==0 || n==0 || n==1){
-        return(1);
-    }
-    else{
-        return(n*fact(n-1, tag-1));
-    }
-}
-float add(int n){
-    int flag;
-    float numerator ,denominator =1.0,sum=0.0;
-    for(int i=1; i<=n; i++){
-        flag = (i%2 == 0)?-1:1;
-        numerator = flag;
-        denominator *= fact(2*i-1,2);
-        sum += numerator/denominator;
-        printf("sum:%.4f, numerator:%.4f, denominator:%.4f, res%.4f:\n", sum, numerator, denominator, numerator/denominator);
-    }
-    return(sum);
->>>>>>> 1788b35824a9807bd83c82600b6a65c9de802dce
 }
 int main(){
     int n;
@@ -50,15 +24,7 @@ int main(){
             printf("Input Error");
             continue;
         }
-<<<<<<< HEAD
         printf("result: %.6f\n",add(n));
     }
     return 0;
 }
-=======
-        printf("%.4f\n",add(n));
-    }
-    return 0;
-}
-
->>>>>>> 1788b35824a9807bd83c82600b6a65c9de802dce
