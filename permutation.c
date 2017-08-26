9a8a253d5a827524a432c89142eb83fc30b3e5c6
hwade <hwade_good@163.com>
2017-08-26 10:18:16 +0800

    可重复数组全排列

--- permutation.c
+++ permutation.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* 
-  题目：长度为n的非重复数组输出全排列
+  题目：长度为n的可重复数组输出全排列
   思路：
     1.n个元素的全排列=（1个元素作为前缀）+（n-1元素进行全排列）；
     2.出口：只有一个元素时，输出整个数组；
@@ -22,14 +22,17 @@ void perm(int list[], int i, int len)
 		return ;
 	}
 	for(int j=i; j<len; j++){
-		swap(list, i, j);
-		perm(list, i+1, len);
-		swap(list, i, j);
+		if(list[i]!=list[j] || i==j){
+			// 若不是同一位值上的值相等，则为数组中重复的值，无需置换再排列
+			swap(list, i, j);
+			perm(list, i+1, len);
+			swap(list, i, j);
+		}
 	}
 }
 int main(void)
 {
-	int A[] = {1,2,3,4,5};
+	int A[] = {1,2,3,2};
 	int len = sizeof(A)/sizeof(int);
 	perm(A,0,len);
 
